8d36f6b1e4356e7dc327da92f04e764f9abb0793
Mega <71318378+harambe-forever@users.noreply.github.com>
2022-03-31 15:51:23 +0300

    Add files via upload

--- personal_analysis.py
+++ personal_analysis.py
@@ -2,10 +2,43 @@ import numpy as np
 import random
 
 
-def main():
-    name = str(input("Enter driver's name:")).lower()
-    find("sqlData.txt", name)
-    print("zort")
+def average_len_of_names(path):
+    print("enter")
+    with open(path, encoding="utf-8") as file:
+        line = file.readline()
+        total_len = 0
+        count = 0
+        while line:
+            if line[0].isdigit() == False:
+                name = line.split(",")[0]
+                count += 1
+                total_len += len(name)
+            line = file.readline()
+        print(total_len/count)
+        print("done")
+
+
+def rdm():
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    words1 = []
+    words2 = []
+    for i in range(1000):
+        iter = random.randint(1, 15)
+        thisWord = []
+        thatWord = []
+        for j in range(0, iter):
+            thisWord.append(alphabet[random.randint(0, 25)])
+            thatWord.append(alphabet[random.randint(0, 25)])
+        words1.append("".join(thisWord))
+        words2.append("".join(thatWord))
+
+    rets = []
+    for k in range(len(words1)):
+        ret = levenshtein(words1[k], words2[k])
+        rets.append(ret)
+    avg = np.mean(rets)
+    print(avg)
 
 
 def levenshtein(seq1, seq2):
@@ -37,61 +70,48 @@ def levenshtein(seq1, seq2):
 
 def find(path, name):
     year = 0
+    flag = True
+    sendIt = False
     with open(path, encoding="utf-8") as file:
         line = file.readline()
         l = []
         while line:
+            l = line.split(",")
+            isYear = line[0].isdigit()
+            lev = levenshtein(l[0], name)
+
             if line[0].isdigit():
                 year = int(line.strip("\n"))
                 pass
-            l = line.split(",")
-            if l[0] == name:
+            if 2 < lev < 5 and flag == True and sendIt == False:
+                print("Did you mean:", l[0], "?(1/0)")
+                dym = int(input())
+                if dym == 1:
+                    flag = False
+                elif dym == 0:
+                    sendIt = True
+            elif 2 < lev < 5 and isYear == 0 and sendIt == False:
+                print("ilk")
+                print(year, l[0], l[1], l[2])
+            elif 2 < lev < 5 and isYear == 0 and sendIt == True and flag == True:
+                print("ikinci")
+                print(year, l[0], l[1], l[2])
+
+            """if l[0] == name:
                 print(year, l[1])
             else:
                 stringMatch = levenshtein(name, l[0].lower())
-                if stringMatch < 5:
+                if 2 < stringMatch < 5:
                     print("did you mean,", l[0].lower())
-                    input()
+                    input()"""
             line = file.readline()
 
 
-def average_len_of_names(path):
-    print("enter")
-    with open(path, encoding="utf-8") as file:
-        line = file.readline()
-        total_len = 0
-        count = 0
-        while line:
-            if line[0].isdigit() == False:
-                name = line.split(",")[0]
-                count += 1
-                total_len += len(name)
-            line = file.readline()
-        print(total_len/count)
-        print("done")
+def main():
+    name = str(input("Enter driver's name:")).lower()
+    find("sqlData.txt", name)
+    print("zort")
 
 
-# main()
-
-"""def rdm():
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    words1 = []
-    words2 = []
-    for i in range(1000):
-        iter = random.randint(1, 15)
-        thisWord = []
-        thatWord = []
-        for j in range(0, iter):
-            thisWord.append(alphabet[random.randint(0, 25)])
-            thatWord.append(alphabet[random.randint(0, 25)])
-        words1.append("".join(thisWord))
-        words2.append("".join(thatWord))
-
-    rets = []
-    for k in range(len(words1)):
-        ret = levenshtein(words1[k], words2[k])
-        rets.append(ret)
-    avg = np.mean(rets)
-    print(avg)
-"""
+main()
+#print(levenshtein("max verstappen", "jos verstappen"))
